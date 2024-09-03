05f9847abf47a17466b1535dc05aa41bb5f14fc1
Victor Abimbola <abimbolavictor3@gmail.com>
2024-09-03 14:32:32 +0100

    'fixes'

--- src/Components/Loader/index.jsx
+++ src/Components/Loader/index.jsx
@@ -1,11 +1,11 @@
-import dlyaLogo from '../../assets/images/d-log.svg'
+import dlyaLogo from '../../assets/png/log.jpg'
 import 'animate.css';
 export const Loader = () => {
     return (
       <div className="w-100 animate__animated animate__pulse">
       <div class="pulseWrapper">
       <div class="pulse">
-        EDD
+        <img src={dlyaLogo} width={'100'} />
       </div>
       </div>
     </div>
